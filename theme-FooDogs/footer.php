0b470fd130f8bc0726fbeef16a9463c8702f7810
ludovic2411 <ludovic.ceccotti@gmail.com>
2018-03-27 16:59:54 +0200

    third

---
+++ theme-FooDogs/footer.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="CSS/footer-css.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Crimson+Text" rel="stylesheet">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!-- retirer ce qui suit au dessus -->
+    <!--le container reprend tout le footer -->
+    <section class="footer-container">
+      <!-- partie gris clair et première grille -->
+      <div class="footer-navigation">
+        <ul class="footer-categories">
+          <li><h3>CATEGORIES</h3></li>
+          <li>COMMUNITY</li>
+          <li>FEATURES</li>
+          <li>LIFESTYLE</li>
+          <li>NUTRITION</li>
+          <li>WELNESS</li>
+        </ul>
+        <ul class="footer-popular-posts">
+          <li><h3>POPULAR POSTS</h3></li>
+          <li>5 REASONS YOUR DOG IS VOMITING</li>
+          <li>TOP 14 FRESH SNACKS FOR DOGS</li>
+          <li>WHY SKINNY DOGS LIVE LONGER</li>
+        </ul>
+        <!-- seconde grille -->
+        <div>
+        <ul class="instagram-title">
+          <li><h3>INSTAGRAM</h3></li>
+        </ul>
+        <ul class="instagram">
+          <li><img class="item-1" src="../theme-FooDogs/pictures/footer-pictures/item-1.jpg" alt="cute dog"></li>
+          <li><img class="item-2" src="../theme-FooDogs/pictures/footer-pictures/item-2.jpg" alt="cute dog"></li>
+          <li><img class="item-3" src="../theme-FooDogs/pictures/footer-pictures/item-3.jpg" alt="cute dog"></li>
+          <li><img class="item-4" src="../theme-FooDogs/pictures/footer-pictures/item-4.jpg" alt="cute dog"></li>
+          <li><img class="item-5" src="../theme-FooDogs/pictures/footer-pictures/item-5.jpg" alt="cute dog"></li>
+          <li><img class="item-6" src="../theme-FooDogs/pictures/footer-pictures/item-6.jpg" alt="cute dog"></li>
+          <li><img class="item-7" src="../theme-FooDogs/pictures/footer-pictures/item-7.jpg" alt="cute dog"></li>
+          <li><img class="item-8" src="../theme-FooDogs/pictures/footer-pictures/item-8.jpg" alt="cute dog"></li>
+          <li><img class="item-9" src="../theme-FooDogs/pictures/footer-pictures/item-9.jpg" alt="cute dog"></li>
+        </ul>
+      </div>
+      </div>
+      <!-- Partie gris foncé -->
+      <div class="footer-social">
+        <ul>
+          <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+          <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+        </ul>
+        <p>2017 © The Farmer's Dog</p>
+      </div>
+    </section>
+    <?php wp_footer(); ?>
+  </body>
+</html>

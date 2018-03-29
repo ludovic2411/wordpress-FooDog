edf3b9e96124c3cac1e6df1fe1696d1afa4ed206
ludovic2411 <ludovic.ceccotti@gmail.com>
2018-03-29 09:18:18 +0200

    fourth

--- theme-FooDogs/footer.php
+++ theme-FooDogs/footer.php
@@ -1,4 +1,6 @@
+<?php
 
+ ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -15,34 +17,34 @@
       <!-- partie gris clair et première grille -->
       <div class="footer-navigation">
         <ul class="footer-categories">
-          <li><h3>CATEGORIES</h3></li>
-          <li>COMMUNITY</li>
-          <li>FEATURES</li>
-          <li>LIFESTYLE</li>
-          <li>NUTRITION</li>
-          <li>WELNESS</li>
+          <li><h3></h3></li>
+          <li><p><a href=""></a></p></li>
+          <li><p><a href=""></a></p></li>
+          <li><p><a href=""></a></p></li>
+          <li><p><a href=""></a></p></li>
+          <li><p><a href=""></a></p></li>
         </ul>
         <ul class="footer-popular-posts">
-          <li><h3>POPULAR POSTS</h3></li>
-          <li>5 REASONS YOUR DOG IS VOMITING</li>
-          <li>TOP 14 FRESH SNACKS FOR DOGS</li>
-          <li>WHY SKINNY DOGS LIVE LONGER</li>
+          <li><h3></h3></li>
+          <li><p><a href="#"><img src="" alt=""></a></p></li>
+          <li><p><a href="#"><img src="" alt=""></a></p></li>
+          <li><p><a href="#"><img src="" alt=""></a></p></li>
         </ul>
         <!-- seconde grille -->
         <div>
         <ul class="instagram-title">
-          <li><h3>INSTAGRAM</h3></li>
+          <li><h3></h3></li>
         </ul>
         <ul class="instagram">
-          <li><img class="item-1" src="../theme-FooDogs/pictures/footer-pictures/item-1.jpg" alt="cute dog"></li>
-          <li><img class="item-2" src="../theme-FooDogs/pictures/footer-pictures/item-2.jpg" alt="cute dog"></li>
-          <li><img class="item-3" src="../theme-FooDogs/pictures/footer-pictures/item-3.jpg" alt="cute dog"></li>
-          <li><img class="item-4" src="../theme-FooDogs/pictures/footer-pictures/item-4.jpg" alt="cute dog"></li>
-          <li><img class="item-5" src="../theme-FooDogs/pictures/footer-pictures/item-5.jpg" alt="cute dog"></li>
-          <li><img class="item-6" src="../theme-FooDogs/pictures/footer-pictures/item-6.jpg" alt="cute dog"></li>
-          <li><img class="item-7" src="../theme-FooDogs/pictures/footer-pictures/item-7.jpg" alt="cute dog"></li>
-          <li><img class="item-8" src="../theme-FooDogs/pictures/footer-pictures/item-8.jpg" alt="cute dog"></li>
-          <li><img class="item-9" src="../theme-FooDogs/pictures/footer-pictures/item-9.jpg" alt="cute dog"></li>
+          <li><img class="left" class="item-1" src="" alt="cute dog"></li>
+          <li><img class="center" class="item-2" src="" alt="cute dog"></li>
+          <li><img class="right" class="item-3" src="" alt="cute dog"></li>
+          <li><img class="left" class="item-4" src="" alt="cute dog"></li>
+          <li><img class="center" class="item-5" src="" alt="cute dog"></li>
+          <li><img class="right" class="item-6" src="" alt="cute dog"></li>
+          <li><img class="left" class="item-7" src="" alt="cute dog"></li>
+          <li><img class="center" class="item-8" src="" alt="cute dog"></li>
+          <li><img class="right" class="item-9" src="" alt="cute dog"></li>
         </ul>
       </div>
       </div>
@@ -53,7 +55,7 @@
           <li><a href="#"><i class="fab fa-twitter"></i></a></li>
           <li><a href="#"><i class="fab fa-instagram"></i></a></li>
         </ul>
-        <p>2017 © The Farmer's Dog</p>
+        <p class="copyright">2018 © Les Grosses Teubs</p>
       </div>
     </section>
     <?php wp_footer(); ?>
---
+++ theme-FooDogs/sidebar.php
@@ -0,0 +1,21 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="CSS/sidebar-css.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Crimson+Text" rel="stylesheet">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<!-- la partie au dessus sera à supprimer à la fin -->
+<section class="sidebar">
+  <ul>
+    
+  </ul>
+</section>
+<!--La partie en dessous sera également à supprimer -->
+  </body>
+</html>

b42371f0927883c754f649897de43e615dd0be3d
Felix Nameche <nameche.felix@gmail.com>
2018-04-09 16:02:27 +0200

    maj header et style menu hamburger

--- theme-FooDogs/header-article.php
+++ theme-FooDogs/header-article.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
         <input class="burger-check" id="burger-check" type="checkbox"><label for="burger-check" class="burger"></label>
         <nav id="navigation1" class="navigation">
             <ul>
@@ -9,11 +8,10 @@
                 <li><a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a></li>
             </ul>
             <ul>
-                <li><a href=community.php>Community</a></li>
+                <li><a href="community.php">Community</a></li>
                 <li><a href="lifestyle.php">Lifestyle</a></li>
                 <li><a href="nutrition.php">Nutrition</a></li>
                 <li><a href="wellness.php">Wellness</a></li>
             </ul>
         </nav>
-</head>
 </html>

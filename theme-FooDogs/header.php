d6a15473dbad2464439840157cf2dacb36d209b2
Felix Nameche <nameche.felix@gmail.com>
2018-04-09 09:37:19 +0200

    ajout header

---
+++ theme-FooDogs/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Les Grosses Teubes" />
+    <link rel="stylesheet" href="header-style.css"  type="text/css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
+</head>
+<body>
+<header>
+    <div class="headertop">
+        <div class="header-sociallink">
+            <ul>
+                <li><i class="fab fa-facebook-f"></i></i></li>
+                <li><i class="fab fa-twitter"></i></li>
+                <li><i class="fab fa-instagram"></i></li>
+                <li><i class="fas fa-search"></i></li>
+            </ul>
+            </div>
+        <div class="header-title">
+            <h1>FooDog</h1>
+        </div>
+    </div>
+
+</header>
